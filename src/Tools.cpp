fc8f62a3ef8cf3ab003e2f408cfea72279d4f056
Matheus Gabriel Werny de Lima <matheusgwdl@protonmail.com>
2019-12-07 11:21:41 +0100

    extended Tools

--- src/Tools.cpp
+++ src/Tools.cpp
@@ -8,6 +8,7 @@
 #include "Poco/MD5Engine.h"
 #include "Poco/DigestStream.h"
 #include "Poco/HMACEngine.h"
+#include "Poco/URI.h"
 
 namespace tools
 {
@@ -537,5 +538,11 @@ namespace tools
 		Poco::HMACEngine<SHA256Engine> hmac{secret_key};
 		hmac.update(str);
 		return Poco::DigestEngine::digestToHex(hmac.digest());
+	}
+
+	std::string Tools::url_parse(const std::string &url)
+	{
+		Poco::URI uri(url);
+		return uri.toString();
 	}
 }
