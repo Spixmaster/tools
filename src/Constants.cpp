c350367f8d5f051fba9b09b4e2b0c84a3ece0f03
Spixmaster <spixmaster528@gmail.com>
2020-01-23 22:13:17 +0100

    enhanced error messages

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -4,8 +4,8 @@
 
 namespace tools
 {
-	const std::string Constants::version = "1.0.9";
-	const std::string Constants::release = "21.Jan.2020";
+	const std::string Constants::version = "1.1.0";
+	const std::string Constants::release = "23.Jan.2020";
 
 	void Constants::file_non_existent(const std::string &file)
 	{
