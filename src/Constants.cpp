4f7c7980455180cf0caae232aca58251b6ac14b8
Spixmaster <spixmaster528@gmail.com>
2020-02-21 12:00:31 +0100

    http client error output fixed

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -4,7 +4,7 @@
 
 namespace tools
 {
-	const std::string Constants::version = "1.1.34";
+	const std::string Constants::version = "1.1.35";
 	const std::string Constants::release = "21.Feb.2020";
 	const std::string Constants::ansi_reset = "\x1B[0m";
 	const std::string Constants::ansi_black = "\x1B[30m";
