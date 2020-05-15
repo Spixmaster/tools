42228b9ae369f18d8121b5d13085147e3efc4bf8
Matheus Gabriel Werny de Lima <matheusgwdl@protonmail.com>
2020-05-15 16:04:58 +0200

    Readme update.

--- include/tools/Tools.h
+++ include/tools/Tools.h
@@ -13,6 +13,9 @@
  * @author Matheus Gabriel Werny de Lima
  * @copyright GNU General Public License v2.0
  * @version
+ * 1.1.31 (15.05.2020)
+ * - Readme update.
+ * @version
  * 1.1.30 (15.05.2020)
  * - Contributing.md update.
  * @version
