40b7c6e41c41b33d692d1f7938ff1c777e3881e0
Spixmaster <spixmaster528@gmail.com>
2020-05-28 11:10:14 +0200

    Comment updated.

--- src/constants/Constants.cpp
+++ src/constants/Constants.cpp
@@ -7,8 +7,8 @@
 
 namespace tools
 {
-	const std::string Constants::version = "1.1.35";
-	const std::string Constants::release = "24.May.2020";
+	const std::string Constants::version = "1.1.36";
+	const std::string Constants::release = "28.May.2020";
 	const std::size_t Constants::max_tmp_err_logs = 10;
 
 	const std::string Constants::ansii_reset = "\x1B[0m";
