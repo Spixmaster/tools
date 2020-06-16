6d744001bc35388a8d7b519788e8023ca81a6c4f
Matheus Gabriel Werny de Lima <matheusgwdl@protonmail.com>
2020-06-16 07:29:34 +0200

    Script update.

--- src/constants/Constants.cpp
+++ src/constants/Constants.cpp
@@ -7,8 +7,8 @@
 
 namespace tools
 {
-	const std::string Constants::version = "1.1.41";
-	const std::string Constants::release = "15.Jun.2020";
+	const std::string Constants::version = "1.1.42";
+	const std::string Constants::release = "16.Jun.2020";
 	const std::size_t Constants::max_tmp_err_logs = 10;
 
 	const std::string Constants::ansii_reset = "\x1B[0m";
