796547462984439c6bbdc64865c5b7cf64ece685
Spixmaster <spixmaster528@gmail.com>
2020-06-25 08:53:20 +0200

    License changed.

--- src/constants/Constants.cpp
+++ src/constants/Constants.cpp
@@ -7,8 +7,8 @@
 
 namespace tools
 {
-	const std::string Constants::version = "1.2.4";
-	const std::string Constants::release = "22.Jun.2020";
+	const std::string Constants::version = "1.2.5";
+	const std::string Constants::release = "25.Jun.2020";
 	const std::size_t Constants::max_tmp_err_logs = 10;
 
 	const std::string Constants::ansii_reset = "\x1B[0m";
