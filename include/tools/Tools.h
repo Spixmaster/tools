37651d15141515da6a7e347b67b6d283eadeb50b
Matheus Gabriel Werny de Lima <matheusgwdl@protonmail.com>
2020-08-12 08:16:15 +0200

    Cmake update.

--- include/tools/Tools.h
+++ include/tools/Tools.h
@@ -13,6 +13,9 @@
  * @author Matheus Gabriel Werny de Lima
  * @copyright Apache-2.0 License
  * @version
+ * 1.2.13 (12.08.2020)
+ * - Cmake update.
+ * @version
  * 1.2.12 (12.08.2020)
  * - Cmake update.
  * @version
